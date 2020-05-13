0130692b2aa6282fdb77da4e93ae2dba42851284
jamesoneill997 <jamesoneill997@gmail.com>
2020-05-13 09:17:44 +0100

    closed open files

--- application/runPy.c
+++ application/runPy.c
@@ -5,18 +5,21 @@
 
 int main(int argc, char *argv[]){
     FILE *pFile = NULL;
+    char name[] = "errorResponse.txt";
     char *filename = "./errorResponse.txt";
     pFile = fopen(filename, "w+");
     
-
-    char command[] = {"python "};
-    strncat(command,argv[1],sizeof(command));
+    char command[] = {"python3 "};
+    strncat(command, argv[1], sizeof(argv[1]));
+    printf("%s\n", command);
+    strncat(command, filename, sizeof(filename));
+    printf("%s\n", command);
     int result = system(command);
 
-    if(result == 256){
-        printf("\n---------------------Suggested solutions from Stack overflow---------------------\n");
+    if(result % 256==0){
+            printf("\n---------------------Suggested solutions from Stack overflow---------------------\n");
     }
-    
 
+    fclose(pFile);
     return 0;
 }
