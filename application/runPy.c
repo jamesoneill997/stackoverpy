ab83136dbb4a4fcfaa507a71fb5a6054d994371c
jamesoneill997 <jamesoneill997@gmail.com>
2020-05-20 21:58:40 +0100

    refactoring code in runPy

--- application/runPy.c
+++ application/runPy.c
@@ -2,25 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 int main(int argc, char *argv[]){
-    FILE *pFile = NULL;
-    char name[] = "errorResponse.txt";
-    char *filename = "errorResponse.txt";
-    pFile = fopen(filename, "w+");
+
+    FILE *fp;
+    int status;
+    //file setup
+    char name[] = "test.py";
+    char command[] = "python3 ";
+
+    strcat(command, name);
+
+    printf("%s\n",command);
+
+    fp = popen(command, "w");
     
-    char command[] = {"python3 "};
-    strcat(command, argv[1]);
-    strcat(command, " > ");
-    printf("%s\n", command);
-    strcat(command, filename);
-    printf("%s\n", command);
-    int result = system(command);
+    pclose(fp);
 
-    if(result % 256==0){
-            printf("\n---------------------Suggested solutions from Stack overflow---------------------\n");
-    }
-
-    fclose(pFile);
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+    
